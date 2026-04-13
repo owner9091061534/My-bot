b96d5da659ebc7d6cfedf6b07d65233bf0709d03
owner9091061534 <s67984364@gmail.com>
2026-04-13 18:09:02 +0600

    Fix deprecation warning by using clientReady

--- index.js
+++ index.js
@@ -2,7 +2,7 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.once('ready', () => {
+client.once('clientReady', () => {
     console.log('Bot is online!');
 });
 
