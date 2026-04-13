bb39c23bdbc825456764d1cec0cfd1ae1edebbda
owner9091061534 <s67984364@gmail.com>
2026-04-13 21:52:20 +0600

    Fix ready event and add error logging

--- index.js
+++ index.js
@@ -2,8 +2,12 @@ const { Client, GatewayIntentBits } = require('discord.js');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-client.once('clientReady', () => {
+client.once('ready', () => {
     console.log('Bot is online!');
+});
+
+client.on('error', (error) => {
+    console.error('Discord client error:', error);
 });
 
 client.on('messageCreate', (message) => {
@@ -13,4 +17,6 @@ client.on('messageCreate', (message) => {
     }
 });
 
-client.login(process.env.TOKEN);
+client.login(process.env.TOKEN).catch((error) => {
+    console.error('Login failed:', error);
+});
