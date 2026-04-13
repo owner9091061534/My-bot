01b74b7660728354d499d8b7dcb828dc46bf25fd
owner9091061534 <s67984364@gmail.com>
2026-04-13 17:46:40 +0600

    first commit

---
+++ index.js
@@ -0,0 +1,16 @@
+const { Client, GatewayIntentBits } = require('discord.js');
+
+const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
+
+client.once('ready', () => {
+    console.log('Bot is online!');
+});
+
+client.on('messageCreate', (message) => {
+    if (message.author.bot) return;
+    if (message.content === '!ping') {
+        message.reply('pong!');
+    }
+});
+
+client.login(process.env.TOKEN);
